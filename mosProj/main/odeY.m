8507af8debea40835754472795574b7b679d7d6f
Oskar André <93657491+oskarandre@users.noreply.github.com>
2023-02-17 12:18:36 +0100

    added x-movement

--- mosProj/main/odeY.m
+++ mosProj/main/odeY.m
@@ -1,1 +1,11 @@
 %Funktion som beskriver skillnaden i Y-led
+
+function dy = odeY(y)
+m = 0.25;
+g = -9.82;
+fk = 0.07;
+theta = 0.1;
+
+dy = m*g*fk*y*sin(theta);
+
+end
--- mosProj/main/yeuler.m
+++ mosProj/main/yeuler.m
@@ -1,1 +1,22 @@
 %Eulers stegmetod för att beräkna ODEY
+
+function [y,v,t] = yeuler(fun,tspan,y0, n)
+
+a = tspan(1);
+b = tspan(2);
+t = linspace(a,b,n);
+h = t(2)-t(1);
+
+y = zeros(1,length(t));
+v = zeros(1,length(t));
+
+% Startvärde
+y(1) = y0;
+
+for i = 1:n-1
+    v(i+1) = v(i) + h*fun(y(i));
+    y(i+1)= y(i) + h*v(i+1);
+    
+end
+
+end
