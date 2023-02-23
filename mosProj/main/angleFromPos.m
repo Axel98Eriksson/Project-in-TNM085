f84dc21084bc81f190b44369bd7750e443741567
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2023-02-23 16:29:17 +0100

    main2 tillagd för att ogge ska kunna se något för att han är blind

--- mosProj/main/angleFromPos.m
+++ mosProj/main/angleFromPos.m
@@ -1,9 +1,11 @@
-function angle = angleFromPos(pos,h,maxTheta,n)
+function angle = angleFromPos(pos,n,maxTheta)
 
-angles = zeros(1,)
+angles = zeros(1,n);
+board = zeros(1,n);
 
-angles = 0+maxTheta:h:pi-maxTheta;
-board = -0.5:h:0.5;
+angles() = 0+maxTheta:pi-maxTheta;
+board() = -0.5:0.5;
+
 angle = 0;
 for i = 1:length(board)
     if board(i) >= pos
